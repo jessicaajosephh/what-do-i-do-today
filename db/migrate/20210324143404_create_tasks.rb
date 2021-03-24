133ff9a7ceefc4fecbe42112941faba177529036
Jessica Joseph <jessicaajosephh@gmail.com>
2021-03-24 10:43:45 -0400

    created tasks and users tables

--- db/migrate/20210324143404_create_tasks.rb
+++ db/migrate/20210324143404_create_tasks.rb
@@ -1,4 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :description
+      t.integer :user_id
+    end
   end
 end
